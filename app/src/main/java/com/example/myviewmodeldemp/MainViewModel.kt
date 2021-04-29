80788adcaac0278568646c0f2f8e8b7fe4edabdd
Furqhan <furqhan1122@gmail.com>
2021-04-29 15:56:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myviewmodeldemp/MainViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.myviewmodeldemp
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import java.nio.charset.Charset
+
+class MainViewModel(val context : Context):ViewModel() {
+
+    private var quoteList: Array<Quotes> = emptyArray()
+    private var index =0
+
+    init {
+        quoteList=loadQuoteFromAsset()
+    }
+
+    private fun loadQuoteFromAsset(): Array<Quotes> {
+        val inputStream = context.assets.open("quotes.json")
+        val size:Int =inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        val json = String(buffer, Charsets.UTF_8)
+        val gson= Gson()
+        return gson.fromJson(json,Array<Quotes>::class.java)
+
+    }
+
+    fun getQuote()=quoteList[index]
+
+    fun nextQuote()=quoteList[++index]
+
+    fun previousQuote()=quoteList[--index]
+}
